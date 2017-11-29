8bda4bd5c2613f70d8e5e8067b99add8221c64c8
ShinDarth <borzifrancesco@gmail.com>
2017-11-29 19:58:11 +0100

    upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TimepickerModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TimepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
